86eeb41938f762c42d946258b65f364ffdbb25cf
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-02-17 19:26:39 +0100

    Update

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# Webpify
-Convertir png y jpg en webp.
+# Webpify 🎨✨
+
+**Webpify** es una herramienta de terminal escrita en **Node.js** que convierte tus imagenes PNG y JPG a **WebP**, un formato de imagen optimizado que ofrece compresion sin perdidas, ideal para la web.
+
+¡Convierte tus fotos en imagenes mas ligeras y rapidas de cargar!
+
+---
+
+## 🚀 Caracteristicas
+
+- **Conversion rapida y eficiente** de imagenes PNG y JPG a WebP.
+- **Facil de usar** a traves de la linea de comandos.
+- Utiliza **Node.js** y **Sharp** para la manipulacion de imagenes.
+- Optimizacion de imagenes para mejorar el rendimiento de tus sitios web.
+
+
+## Indice
+
+- [Visualizacion](#Visualizacion)
+- [Tecnologias](#tecnologias)
+- [Instalacion](#instalacion)
+- [Creditos](#creditos)
+
+<br>
+
+## Visualizacion
+
+<br>
+
+<!-- ![Home](/doc/img/Home.png) -->
+
+<br>
+
+## Tecnologias
+
+<br>
+
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40" />
+  </a>
+
+<br>
+
+## Instalacion
+
+<br>
+
+## Creditos
+
+<br>
+
+[Alejandro Barrionuevo Rosado](https://github.com/Alejandro-BR)
+
+<br>
--- convert.js
+++ convert.js
@@ -1,41 +1,51 @@
-import sharp from 'sharp';
-import path from 'path';
-import fs from 'fs-extra';
-import chalk from 'chalk';
+import sharp from "sharp";
+import path from "path";
+import fs from "fs-extra";
+import chalk from "chalk";
 
 async function convertToWebP(inputPath, outputPath) {
-    try {
-        await sharp(inputPath)
-            .webp({ quality: 100 })
-            .toFile(outputPath);
-        console.log(chalk.green(`✅ Imagen convertida: ${path.basename(outputPath)}`));
-    } catch (err) {
-        console.error(chalk.red(`❌ Error al convertir ${path.basename(inputPath)}:`), err);
-    }
+  try {
+    await sharp(inputPath).webp({ quality: 100 }).toFile(outputPath);
+    console.log(
+      chalk.green(`✅ Imagen convertida: ${path.basename(outputPath)}`)
+    );
+  } catch (err) {
+    console.error(
+      chalk.red(`❌ Error al convertir ${path.basename(inputPath)}:`),
+      err
+    );
+  }
 }
 
 export async function convertImagesInDirectory(directoryPath) {
-    try {
-        const files = await fs.readdir(directoryPath);
-        const imageFiles = files.filter(file => file.match(/\.(png|jpe?g)$/i));
+  try {
+    const files = await fs.readdir(directoryPath);
+    const imageFiles = files.filter((file) => file.match(/\.(png|jpe?g)$/i));
 
-        if (imageFiles.length === 0) {
-            console.log(chalk.yellow("⚠️ No se encontraron imágenes PNG o JPG en el directorio."));
-            return;
-        }
-
-        console.log(chalk.blue(`🔄 Procesando ${imageFiles.length} imágenes...\n`));
-
-        for (const file of imageFiles) {
-            const inputPath = path.join(directoryPath, file);
-            const outputPath = path.join(directoryPath, `${path.parse(file).name}.webp`);
-            await convertToWebP(inputPath, outputPath);
-        }
-
-        console.log(chalk.green("🎉 Conversión completada con éxito."));
-    } catch (err) {
-        console.error(chalk.red("❌ Error al leer el directorio:"), err);
-    } finally {
-      console.log("\n");
+    if (imageFiles.length === 0) {
+      console.log(
+        chalk.yellow(
+          "⚠️ No se encontraron imágenes PNG o JPG en el directorio."
+        )
+      );
+      return;
     }
+
+    console.log(chalk.blue(`🔄 Procesando ${imageFiles.length} imágenes...\n`));
+
+    for (const file of imageFiles) {
+      const inputPath = path.join(directoryPath, file);
+      const outputPath = path.join(
+        directoryPath,
+        `${path.parse(file).name}.webp`
+      );
+      await convertToWebP(inputPath, outputPath);
+    }
+
+    console.log(chalk.green("🎉 Conversión completada con éxito."));
+  } catch (err) {
+    console.error(chalk.red("❌ Error al leer el directorio:"), err);
+  } finally {
+    console.log("\n");
+  }
 }
